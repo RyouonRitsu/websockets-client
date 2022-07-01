ae11eddb86e27c86be22078b207c1cdba8a2e37a
RyouonRitsu <zhou.hongxi0903@qq.com>
2022-07-01 16:16:28 +0800

    设定依赖

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,13 @@ repositories {
     mavenCentral()
 }
 
+val ktor_version: String by project
 
+dependencies {
+    implementation("io.ktor:ktor-client-core:$ktor_version")
+    implementation("io.ktor:ktor-client-cio:$ktor_version")
+    implementation("io.ktor:ktor-client-websockets:$ktor_version")
+}
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
