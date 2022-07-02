83cb3e0b39470e15bb507da8d61f90d725ec647e
RyouonRitsu <zhou.hongxi0903@qq.com>
2022-07-03 01:10:12 +0800

    私聊完成

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,21 +2,45 @@ import io.ktor.client.*
 import io.ktor.client.plugins.websocket.*
 import io.ktor.http.*
 import io.ktor.websocket.*
+import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.cancelAndJoin
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 fun main() {
+    val cmdList = listOf("chat", "whisper", "exit")
     val client = HttpClient {
         install(WebSockets)
     }
+    println("Welcome to use this application!")
     runBlocking {
-        client.webSocket(method = HttpMethod.Get, host = "127.0.0.1", port = 8080, path = "/chat") {
-            val messageOutputRoutine = launch { outputMessages() }
-            val userInputRoutine = launch { inputMessages() }
+        while (true) {
+            println("To start communicating, please input your command like: ${cmdList.joinToString(", ")}!")
+            val cmd = readLine() ?: ""
+            if (cmd.equals("exit", true)) break
+            else {
+                when (cmd) {
+                    "chat" -> {
+                        client.webSocket(method = HttpMethod.Get, host = "127.0.0.1", port = 8080, path = "/chat") {
+                            val messageOutputRoutine = launch { outputMessages() }
+                            val userInputRoutine = launch { inputMessages() }
 
-            userInputRoutine.join() // Wait for completion; either "exit" or error
-            messageOutputRoutine.cancelAndJoin()
+                            userInputRoutine.join() // Wait for completion; either "exit" or error
+                            messageOutputRoutine.cancelAndJoin()
+                        }
+                    }
+                    "whisper" -> {
+                        client.webSocket(method = HttpMethod.Get, host = "127.0.0.1", port = 8080, path = "/whisper") {
+                            val messageOutputRoutine = launch { outputMessages() }
+                            val userInputRoutine = launch { inputMessages() }
+
+                            userInputRoutine.join() // Wait for completion; either "exit" or error
+                            messageOutputRoutine.cancelAndJoin()
+                        }
+                    }
+                    else -> println("sys: command not found: $cmd")
+                }
+            }
         }
     }
     client.close()
@@ -31,6 +55,8 @@ suspend fun DefaultClientWebSocketSession.outputMessages() {
                 else -> continue
             }
         }
+    } catch (e: CancellationException) {
+        println("sys: receiving cancelled, back to command mode")
     } catch (e: Exception) {
         println("Error while receiving: ${e.localizedMessage}")
     }
